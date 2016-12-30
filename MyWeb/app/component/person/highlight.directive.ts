653c20fbeb065ff7f272094d68974c3a5bc6a268
FredJiangFei <jiangf@shinetechchina.com>
2016-12-30 22:19:32 +0800

    person partial module

--- MyWeb/app/component/person/highlight.directive.ts
+++ MyWeb/app/component/person/highlight.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, ElementRef, Renderer } from '@angular/core';
 @Directive({ selector: '[highlight], input' })
-/** Highlight the attached element or an InputElement in blue */
+
 export class HighlightDirective {
     constructor(renderer: Renderer, el: ElementRef) {
         renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'powderblue');
